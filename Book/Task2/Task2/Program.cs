349c8a8871a58e9c477994160b0f641744768285
VLiamin <LyaminVA@yandex.ru>
2021-07-11 02:47:08 +0300

    added task2

---
+++ Book/Task2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 7;
+            Console.WriteLine(a + b);
+        }
+    }
+}
