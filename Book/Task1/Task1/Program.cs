84303565fb369a7ca2c8a2cdab5d402506cd525a
VLiamin <LyaminVA@yandex.ru>
2021-07-11 03:00:20 +0300

    added task1

--- Book/Task1/Task1/Program.cs
+++ Book/Task1/Task1/Program.cs
@@ -7,8 +7,6 @@ namespace Task1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("fjfjjf");
-            Console.WriteLine(3 + 5);
         }
     }
 }
