1583d6f5cb17511e93c69daafa6eb69313548db6
VLiamin <LyaminVA@yandex.ru>
2021-07-12 02:18:43 +0300

    added Task3

---
+++ Book/Task3/Task3/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class BubbleSort : Sort
+    {
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Book/Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array1 = {8, 2, 3, 6, 1, 9 };
+            int[] array2 = { 8, 2, 3, 6, 1, 9 };
+
+            Sort sort = new BubbleSort();
+            sort.Sort(array1);
+            sort = new SortByInserts();
+            sort.Sort(array2);
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write(array1[i] + " ");
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.Write(array2[i] + " ");
+            }
+        }
+    }
+}
